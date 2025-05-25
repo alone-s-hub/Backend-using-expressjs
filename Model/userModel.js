8d87a7e2fc3f0c2b9785a386fcbf0cc0400d6a8e
Haris Khan <hariskhan01.gsm@gmail.com>
2025-05-25 21:27:06 +0500

    main

--- Model/userModel.js
+++ Model/userModel.js
@@ -13,6 +13,10 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  pssword:{
+    type: String,
+    required: true,
+  }
 });
 
 export default mongoose.model("users", userSchema);
